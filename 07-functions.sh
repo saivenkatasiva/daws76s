b496fba0de9ed135d2bb6f5f253f8a994e87c739
msaivenkatasiva <msaivenkatasiva@gmal.com>
2025-02-28 22:31:42 +0530

    arrays

--- 07-functions.sh
+++ 07-functions.sh
@@ -4,6 +4,7 @@ validate(){
 if [ $1 ne 0 ]
 then
 echo "$2 installation failed"
+exit 127
 else
 echo "$2 installation SUCCESS"
 fi
@@ -14,6 +15,7 @@ if [ $ID -ne 0 ]
 then
 echo "your not root user, Please run with root user access"
 else
+exit 127
 echo "your root user"
 fi
 
