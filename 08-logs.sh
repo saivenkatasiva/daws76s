e7d068ab0d8f54aa316e67f1ef338d84d2b19797
msaivenkatasiva <msaivenkatasiva@gmal.com>
2025-02-28 23:31:15 +0530

    arrays

---
+++ 08-logs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ID=$(id -u)
+timestamp=$(date +%F-%H-%M-%S)
+logfile="/tmp/$0-$timestamp-log"
+
+validation(){
+if [ $1 -ne 0 ]
+then
+echo "$2 installtion----failed"
+exit 1
+echo "SQL installtion----passed"
+fi
+}
+if [ id -ne 0 ]
+then
+echo "your not root user, please access with root user "
+exit 1
+else
+echo "yoiur root user"
+fi
+
+git install mysql -y $>> $logfile
+validate $? "SQL" 
